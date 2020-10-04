fbaf086bc4c15d57fb83a92e889d15f193c7dbc0
Beatnik14 <sabatevdorashvili99@gmail.com>
2020-10-04 12:56:35 +0400

    fix local storage manipulation

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ const search = document.getElementById('search');
 const deleteBtn = document.getElementById('deleteBtn');
 const deleteInput = document.getElementById('deleteInput');
 
-// const statement1 = new Statement('ჩუქდება თეთრი კნუტი, 1 თვის', 'ლუკა', '599931926', 'თბილისი', 'არის ძალიან საყვარელი', null, 0, 'https://envato-shoebox-0.imgix.net/2d04/0e8a-f427-11e1-952c-842b2b692e1a/2100817-001.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=700&s=1110052f8c6d4715316ccbdd8eaad0fb');
-// let statementsArray = [statement1]
-
-// localStorage.setItem('statements',JSON.stringify(statementsArray))
-// let emptyArray = [];
-// if (localStorage.getItem('statements') === null) {
-//     localStorage.setItem('statements',JSON.stringify(emptyArray))
-// }
+let emptyArray = [];
+if (localStorage.getItem('statements') === '') {
+    localStorage.setItem('statements',JSON.stringify(emptyArray))
+}
 const statementsAsString = localStorage.getItem('statements');
 let statementsAsArray = JSON.parse(statementsAsString);
 console.log(statementsAsString);
